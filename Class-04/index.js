e50836864a3b1eaabcad1058e71e54d73d5a00fb
Talha Hayat <taqimadakhail9999@gmail.com>
2025-06-03 00:36:47 +0500

    some errors

---
+++ Class-04/index.js
@@ -0,0 +1,28 @@
+import express from "express"
+import mongoose, { connect } from "mongoose"
+import User from "./Model/user.js"
+
+const app = express()
+
+const PORT = 5000
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://talhahayat9999:talha@cluster0.rklwnhx.mongodb.net/")
+mongoose.connection.on("connected",()=>{console.log("database connected")})
+mongoose.connection.on("error",(error)=>{console.log(error.message)})
+
+app.get("/",()=>{
+    console.log("Get")
+})
+
+
+
+app.post("/",(request,response)=>{
+    response.send("chala")
+    console.log(request.body)
+    User.create(request.body)
+})
+
+app.listen(PORT,()=>{
+    console.log("Listening")
+})
