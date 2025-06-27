0f44327d5f44560a58bfe2e310fbefd5a0fe3ff4
Talha Hayat <taqimadakhail9999@gmail.com>
2025-06-27 08:33:57 +0500

    Add files via upload

---
+++ Class-09/Backend/Frontend/src/pages/Products/ListProduct.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProductRecentOrders from '../.././components/ecommerce/ProductRecentOrders'
+
+
+const Product = () => {
+  return (
+    <div>
+        <ProductRecentOrders />
+    </div>
+  )
+}
+
+export default Product
